ac44871cfa36768f98658d84d30a3e25224648a4
Zil Norvilis <zilasino27@gmail.com>
2021-03-23 14:35:40 +0100

    Adds game_loop, choose_position and make_move methods

--- bin/main.rb
+++ bin/main.rb
@@ -3,10 +3,17 @@
 class TicTacToe
     def initialize
       @board = Array.new(9, " ")
-      # self.get_names
+      self.get_names
+        self.set_current_player
+        self.game_loop
     end
 
     def game_loop
+        loop do
+            self.display_board
+            self.choose_position
+            self.set_current_player
+        end
     end
 
     def display_board
@@ -16,13 +23,29 @@ class TicTacToe
       puts " #{@board[6]} | #{@board[7]} | #{@board[8]}"
     end
 
+    def make_move(position)
+        @board[position] = 'X' if @current_player == @player_1
+        @board[position] = 'O' if @current_player == @player_2
+    end
+
+    def choose_position
+        puts 'Choose position to add your symbol to : 1-9'
+        choice = gets.chomp.to_i-1
+        if self.valid_move?(choice)
+            make_move(choice)
+        else
+            puts 'Invalid move. Try again!'
+            self.choose_position
+        end
+    end
+
     def set_current_player
         if @current_player == @player_1
-            @current_player == @player_2
+            @current_player = @player_2
         elsif @current_player == @player_2
-            @current_player == @player_1
+            @current_player = @player_1
         else
-            @current_player = [@player_1, @player_2]
+            @current_player = [@player_1, @player_2].sample
         end
     end
 
@@ -39,8 +62,8 @@ class TicTacToe
      @board[position] == "X" || @board[position] == "O"
     end
 
-    def valid_move?(position)
-     !position_taken?(position) && @board[position] == " "
+    def valid_move?(int)
+     !position_taken?(int) && @board[int] == " "
     end
 
 end
