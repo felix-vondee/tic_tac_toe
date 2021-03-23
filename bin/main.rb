429d6d062b8e592061333a103fa1cfb1d16a2696
felix_vondee <felixvondee8@gmail.com>
2021-03-23 14:55:55 +0200

    Adds position_taken? method

--- bin/main.rb
+++ bin/main.rb
@@ -2,15 +2,30 @@
 
 class TicTacToe
     def initialize
-      self.get_names
+      @board = Array.new(9,"")
+      # self.get_names
+    end
+
+    def display_board 
+      puts " #{@board[0]} | #{@board[1]} | #{@board[2]}"
+      puts " #{@board[3]} | #{@board[4]} | #{@board[5]}"
+      puts " #{@board[6]} | #{@board[7]} | #{@board[8]}"
     end
 
     def get_names
-      puts "Enter your name for player 1 "
+      puts "Enter your name for player 1 (X) "
       @player_1 = gets.chomp 
 
-      puts "Enter your name for player 2"
+      puts "Enter your name for player 2 (O)"
       @player_2 = gets.chomp 
+
+    end
+
+    def position_taken?(position)
+     
+     @board[position] == "X" || @board[position] == "O"
+     
+    
 
     end
 end
