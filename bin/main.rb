63df361a1f27a51fdc582bca6f212ff37427c06f
felix_vondee <felixvondee8@gmail.com>
2021-03-23 14:21:14 +0200

    Add method to get names of players

--- bin/main.rb
+++ bin/main.rb
@@ -2,6 +2,17 @@
 
 class TicTacToe
     def initialize
-        
+      self.get_names
+    end
+
+    def get_names
+      puts "Enter your name for player 1 "
+      @player_1 = gets.chomp 
+
+      puts "Enter your name for player 2"
+      @player_2 = gets.chomp 
+
     end
 end
+
+TicTacToe.new
